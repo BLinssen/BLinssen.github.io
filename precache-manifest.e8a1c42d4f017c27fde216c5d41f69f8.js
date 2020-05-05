49715940d1b6573688fa2d421060f6c570abbac5
Bram Linssen <BLinssen95@Gmail.com>
2020-05-05 19:20:03 +0200

    2020 Update

---
+++ precache-manifest.e8a1c42d4f017c27fde216c5d41f69f8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "73e72c096f5aad942867cd3130430768",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f6e26b05c86ce0673ac6",
+    "url": "/static/css/main.b9e0acb4.chunk.css"
+  },
+  {
+    "revision": "d73ea675c45b43a980ec",
+    "url": "/static/js/2.5c20e2d1.chunk.js"
+  },
+  {
+    "revision": "3143274d7d26932c98894a52e75c487a",
+    "url": "/static/js/2.5c20e2d1.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f6e26b05c86ce0673ac6",
+    "url": "/static/js/main.80c0a464.chunk.js"
+  },
+  {
+    "revision": "0adeb7b1d8f47cd9bf9f",
+    "url": "/static/js/runtime-main.71617d02.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
